151f2d500c7a410a7b2a98315b1f3065b79101d3
Jesús Trancoso Jiménez <j.trancoso@altostratus.es>
2024-05-14 14:33:22 +0200

    api control, .env

--- main.go
+++ main.go
@@ -2,24 +2,33 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
+	"github.com/joho/godotenv"
 	"net/http"
+	"os"
 	"strings"
 )
 
 // Middleware para verificar el origen de la solicitud
 func verifyOrigin(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		url := os.Getenv("DOMAIN_URL")
+		validApiKey := os.Getenv("API_KEY_GENBOX")
+
 		// Verifica si la solicitud proviene de un navegador
 		if strings.Contains(r.Header.Get("User-Agent"), "Mozilla") {
 			// Verifica si el dominio de origen es el permitido
-			if r.Header.Get("Origin") == "http://dominio-permitido.com" {
-				next.ServeHTTP(w, r)
+			if r.Header.Get("Origin") != url {
+				http.Error(w, "Forbidden", http.StatusForbidden)
+				return
+			}
+			apiKey := r.Header.Get("X-API-Key")
+			if apiKey != validApiKey {
+				http.Error(w, "Unauthorized", http.StatusUnauthorized)
 				return
 			}
 		}
-
-		// Si no cumple con los criterios, devolver un error de acceso prohibido
-		http.Error(w, "Acceso prohibido", http.StatusForbidden)
+		next.ServeHTTP(w, r)
 	})
 }
 
@@ -48,10 +57,14 @@ func protectedEndpoint(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	// Manejador del endpoint protegido
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Error loading .env file")
+	}
 	http.Handle("/protected", verifyOrigin(http.HandlerFunc(protectedEndpoint)))
 
 	// Inicia el servidor en el puerto 8080
-	err := http.ListenAndServe(":8080", nil)
+	err = http.ListenAndServe(":8080", nil)
 	if err != nil {
 		return
 	}
